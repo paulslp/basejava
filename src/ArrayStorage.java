05bb456682cdc1c5e870b41a4eab70b615d8e55e
paulslp <paulslp@yandex.ru>
2018-08-01 12:20:26 +0400

    Класс ArrayStorage реализован

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,62 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i=0;i<this.size();i++)
+        {
+            storage[i]=null;
+        }
     }
 
+
     void save(Resume r) {
+        for (int i=0;i<10000;i++)
+        {
+            if (storage[i]==null) {
+                storage[i]=r;
+                break;
+            }
+        }
+
+
     }
 
     Resume get(String uuid) {
+        for (int i=0;i<this.size();i++)
+        {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int i;
+        for (i=0;i<this.size();i++)
+        {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j=i;j<this.size();j++) {
+                    storage[j] = storage[j + 1];
+                }
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, this.size());
+        //return new Resume[0];
     }
 
     int size() {
-        return 0;
+        int i=0;
+        while (storage[i]!=null)
+        {
+            i=i+1;
+        }
+        return i;
     }
 }
