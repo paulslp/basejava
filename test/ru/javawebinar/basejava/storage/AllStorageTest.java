e29482c711431ea44b3960b8bcc9bb3ab3b61d09
PaulS <paulslp@yandex.ru>
2018-09-23 00:15:39 +0400

    allstoragetest

--- test/ru/javawebinar/basejava/storage/AllStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -4,8 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-        {
+@Suite.SuiteClasses({
                 ArrayStorageTest.class,
                 SortedArrayStorageTest.class,
                 ListStorageTest.class,
