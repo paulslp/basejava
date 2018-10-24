083e33ed441e23bb09b6a440e514a41fe38435f2
paulslp <paulslp@yandex.ru>
2018-10-24 16:46:44 +0400

    lesson 11 commit2 DeadLockExample without IDEA warning

--- src/ru/javawebinar/basejava/DeadLockExample.java
+++ src/ru/javawebinar/basejava/DeadLockExample.java
@@ -7,46 +7,41 @@ public class DeadLockExample {
         final Object objectA = new Object();
         final Object objectB = new Object();
 
-        Object o = new Object();
-        Thread threadA = new Thread() {
-            public void run() {
-                synchronized (objectA) {
-                    System.out.println(Thread.currentThread().getName() + ":locked ObjectA");
+        Thread threadA = new Thread(() -> {
+            synchronized (objectA) {
+                System.out.println(Thread.currentThread().getName() + ":locked ObjectA");
 
-                    try {
-                        Thread.currentThread().sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    synchronized (objectB) {
-                        System.out.println(Thread.currentThread().getName() + ":locked ObjectB");
-                    }
-
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
-            }
-        };
-
-        Thread threadB = new Thread() {
-            public void run() {
                 synchronized (objectB) {
                     System.out.println(Thread.currentThread().getName() + ":locked ObjectB");
-
-                    try {
-                        Thread.currentThread().sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    synchronized (objectA) {
-                        System.out.println(Thread.currentThread().getName() + ":locked ObjectA");
-                    }
-
                 }
 
             }
-        };
+
+        });
+
+        Thread threadB = new Thread(() -> {
+            synchronized (objectB) {
+                System.out.println(Thread.currentThread().getName() + ":locked ObjectB");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (objectA) {
+                    System.out.println(Thread.currentThread().getName() + ":locked ObjectA");
+                }
+
+            }
+
+        });
 
         threadA.start();
 
