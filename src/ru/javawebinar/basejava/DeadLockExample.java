913221f7756567241e2ae9cc2e7bedcf913d7363
paulslp <paulslp@yandex.ru>
2018-10-25 08:53:57 +0400

    lesson 11 commit3 DeadLockExample creating void method

--- src/ru/javawebinar/basejava/DeadLockExample.java
+++ src/ru/javawebinar/basejava/DeadLockExample.java
@@ -2,50 +2,32 @@ package ru.javawebinar.basejava;
 
 public class DeadLockExample {
 
+
+    static void method(Object objectA, Object objectB) {
+        synchronized (objectA) {
+            System.out.println(Thread.currentThread().getName() + ":locked ObjectA");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            synchronized (objectB) {
+                System.out.println(Thread.currentThread().getName() + ":locked ObjectB");
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         final Object objectA = new Object();
         final Object objectB = new Object();
 
-        Thread threadA = new Thread(() -> {
-            synchronized (objectA) {
-                System.out.println(Thread.currentThread().getName() + ":locked ObjectA");
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                synchronized (objectB) {
-                    System.out.println(Thread.currentThread().getName() + ":locked ObjectB");
-                }
-
-            }
-
-        });
-
-        Thread threadB = new Thread(() -> {
-            synchronized (objectB) {
-                System.out.println(Thread.currentThread().getName() + ":locked ObjectB");
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                synchronized (objectA) {
-                    System.out.println(Thread.currentThread().getName() + ":locked ObjectA");
-                }
-
-            }
-
-        });
+        Thread threadA = new Thread(() -> method(objectA, objectB));
+        Thread threadB = new Thread(() -> method(objectB, objectA));
 
         threadA.start();
-
         threadB.start();
-        System.out.println("End");
+
     }
 }
